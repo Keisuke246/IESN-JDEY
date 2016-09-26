c010806a4efc217cec576de99c2ad00e79cadda8
Jdey <keisuke246@hotmail.com>
2016-09-26 12:21:03 +0200

    Début labo 3

--- AppLabo3/AppLabo3/MainPage.xaml.cs
+++ AppLabo3/AppLabo3/MainPage.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Model;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -22,9 +23,15 @@ namespace AppLabo3
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private List<Contact> lstContacts = new List<Contact>()
+        {
+            new Contact("Charlier","Isabelle","isabelle.charlier@henallux.be"),
+            new Contact("Scholtes","Samuel","samuel.scholtes@henallux.be")
+        };
         public MainPage()
         {
             this.InitializeComponent();
+            DataContext = lstContacts;
         }
     }
 }
