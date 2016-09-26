6fd142f0cd0ba40c437f0f3db9a1c715587fed56
Jdey <keisuke246@hotmail.com>
2016-09-26 08:57:39 +0200

    Ajout du Labo2

---
+++ Labo2/Labo2/Person.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labo2
+{
+    public abstract class Person
+    {
+        private String name;
+        private String lastName;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                name = value;
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return lastname;
+            }
+
+            set
+            {
+                lastname = value;
+            }
+        }
+        public Person(String name, String lastName)
+        {
+            name = Name;
+            lastName = LastName;
+        }
+    }
+}
