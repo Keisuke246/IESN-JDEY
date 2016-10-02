c56bef5ad599d4a36589f6a92854d625bde3a13a
Keisuke246 <Keisuke246@hotmail.com>
2016-10-02 15:26:44 +0200

    Labo 3 version home (création de la vue Contact)

---
+++ WpfApplication1/WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Model;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        private List<Contact> contacts = new List<Contact>()
+        {
+            new Contact("Charlier","Isabelle","isabelle.charlier@henallux.be"),
+            new Contact("Scholtes","Samuel","samuel.scholtes@henallux.be")
+        };
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = contacts;
+        }
+    }
+}
