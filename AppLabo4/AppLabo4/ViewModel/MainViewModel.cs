21811110e93660626f19fc00f8d8f8ee43ce7faa
Jdey <keisuke246@hotmail.com>
2016-10-03 12:18:35 +0200

    Page 8

---
+++ AppLabo4/AppLabo4/ViewModel/MainViewModel.cs
@@ -0,0 +1,54 @@
+﻿using AppLabo4.Model;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Ioc;
+using GalaSoft.MvvmLight.Views;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppLabo4.ViewModel
+{
+    public class MainViewModel : ViewModelBase, INotifyPropertyChanged
+    {
+        private ObservableCollection<Student> _students;
+
+        public ObservableCollection<Student> Students
+        {
+            get { return _students; }
+            set
+            {
+                _students = value;
+                RaisePropertyChanged("Students");
+            }
+        }
+        public MainViewModel()
+        {
+            Students = new ObservableCollection<Student>(AllStudents.GetAllStudents());
+        }
+        private Student _selectedStudent;
+
+        public Student SelectedStudent
+        {
+            get { return _selectedStudent; }
+            set {
+                    _selectedStudent = value;
+                    if (_selectedStudent != null)
+                    {
+                        RaisePropertyChanged("SelectedStudent");
+                    }
+                }
+        }
+        private INavigationService _navigationService;
+        [PreferredConstructor]
+        public MainViewModel(INavigationService navigationService)
+        {
+            _navigationService = navigationService;
+            Students = new ObservableCollection<Student>(AllStudents.GetAllStudents());
+        }
+
+    }
+}
