1bbbf54792f9e79fb8b7ca741f4925f8635901b8
Keisuke246 <Keisuke246@hotmail.com>
2017-05-05 19:46:37 +0200

    Retiré attribut age

--- AppLabo4/AppLabo4/Model/Student.cs
+++ AppLabo4/AppLabo4/Model/Student.cs
@@ -9,12 +9,10 @@ namespace AppLabo4.Model
     public class Student
     {
         public String Name { get; set; }
-        public int Age { get; set; }
 
         public Student(String name, int age)
         {
             Name = name;
-            Age = age;
         }
     }
 }
